929a612b1843a59ad16883143189b20af561b8e5
Anuvardhini T <anuvardhini2105@gmail.com>
2026-09-06 19:36:03 +0530

    Deleted backend directory which has mistakes

--- backend/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { extractFromText } = require('../controllers/aiController');
-
-const router = express.Router();
-
-router.post('/extract', extractFromText);
-
-module.exports = router;
